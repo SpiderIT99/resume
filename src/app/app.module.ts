07d3f1320ffa24a6ecc3e91f9b517f0c54ec9152
Dominik <spiderqwerty@gmail.com>
2023-06-09 19:56:22 +0200

    created all needed elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
+import { CoursesComponent } from './courses/courses.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    AboutMeComponent,
+    ExperienceComponent,
+    EducationComponent,
+    CoursesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
