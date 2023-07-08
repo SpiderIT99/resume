0a897476f69f27d13fe401594cfa80c9b0b011c9
Dominik <spiderqwerty@gmail.com>
2023-07-08 17:52:48 +0200

    Removal of unnecessary solutions that was necessary to submit the task for study

--- src/app/Model/popup.model.ts
+++
@@ -1,5 +0,0 @@
-export class Popup {
-    name: string = '';
-    info: string = '';
-    alert: string = '';
-}
--- src/app/View/app.module.ts
+++ src/app/View/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
-import { AboutMeComponent } from './components/about-me/about-me.component';
-import { ExperienceComponent } from './components/experience/experience.component';
-import { EducationComponent } from './components/education/education.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { CoursesAndLanguagesComponent } from './components/courses-and-languages/courses-and-languages.component';
-import { CookiesComponent } from './components/cookies/cookies.component';
+import { NavigationComponent } from '../components/navigation/navigation.component';
+import { AboutMeComponent } from '../components/about-me/about-me.component';
+import { ExperienceComponent } from '../components/experience/experience.component';
+import { EducationComponent } from '../components/education/education.component';
+import { FooterComponent } from '../components/footer/footer.component';
+import { CoursesAndLanguagesComponent } from '../components/courses-and-languages/courses-and-languages.component';
+import { CookiesComponent } from '../components/cookies/cookies.component';
 
 @NgModule({
   declarations: [
